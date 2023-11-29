16866de18fc9c874ac086d8b74121855cb3c167f
ruanjanau <rhuanjanau531@gmail.com>
2023-11-29 16:45:18 -0400

    feat create address page

---
+++ lib/src/app/view/address_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddressPage extends StatelessWidget {
+  const AddressPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Address Page'),
+      ),
+      body: Container(),
+    );
+  }
+}
