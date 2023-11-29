b4c3229f572b3264e6c73c1704b694650f748520
ruanjanau <rhuanjanau531@gmail.com>
2023-11-29 17:09:27 -0400

    feat create address entity

---
+++ lib/src/app/domain/entities/address_entity.dart
@@ -0,0 +1,25 @@
+class AddressEntity {
+  final String cep;
+  final String logradouro;
+  final String complemento;
+  final String bairro;
+  final String localidade;
+  final String uf;
+  final String ibge;
+  final String gia;
+  final String ddd;
+  final String siafi;
+
+  AddressEntity({
+    required this.cep,
+    required this.logradouro,
+    required this.complemento,
+    required this.bairro,
+    required this.localidade,
+    required this.uf,
+    required this.ibge,
+    required this.gia,
+    required this.ddd,
+    required this.siafi,
+  });
+}
