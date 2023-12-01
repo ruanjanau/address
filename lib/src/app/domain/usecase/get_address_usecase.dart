82da16c4daf2cc5adafe61f98fd8131d05ea18f1
ruanjanau <rhuanjanau531@gmail.com>
2023-12-01 18:25:57 -0400

    alter get address usecase

--- lib/src/app/domain/usecase/get_address_usecase.dart
+++ lib/src/app/domain/usecase/get_address_usecase.dart
@@ -1,11 +1,11 @@
-import 'package:address/src/app/domain/repository/repository.dart';
+import '../repository/repository.dart';
 
 import '../entities/entity.dart';
 import '../error/errors.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class IGetAddressUsecase {
-  Future<Either<AddressError, AddressEntity>> call(String address);
+  Future<Either<AddressError, List<AddressEntity>>> call(String address);
 }
 
 class GetAddressUsecase implements IGetAddressUsecase {
@@ -14,7 +14,7 @@ class GetAddressUsecase implements IGetAddressUsecase {
   GetAddressUsecase(this.repository);
 
   @override
-  Future<Either<AddressError, AddressEntity>> call(String address) async {
-    return await repository.call(address);
+  Future<Either<AddressError, List<AddressEntity>>> call(String address) async {
+    return await repository.getAll(address);
   }
 }
