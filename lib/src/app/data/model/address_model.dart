2485f75c469a19e185b9ede76f6b27872011488c
ruanjanau <rhuanjanau531@gmail.com>
2023-12-01 18:24:20 -0400

    feat create address model

---
+++ lib/src/app/data/model/address_model.dart
@@ -0,0 +1,57 @@
+import 'package:address/src/app/domain/entities/entity.dart';
+
+class AddressModel {
+  final String? cep;
+  final String? logradouro;
+  final String? complemento;
+  final String? bairro;
+  final String? localidade;
+  final String? uf;
+  final String? ibge;
+  final String? gia;
+  final String? ddd;
+  final String? siafi;
+
+  AddressModel({
+    this.cep,
+    this.logradouro,
+    this.complemento,
+    this.bairro,
+    this.localidade,
+    this.uf,
+    this.ibge,
+    this.gia,
+    this.ddd,
+    this.siafi,
+  });
+
+  factory AddressModel.fromJson(dynamic map) {
+    return AddressModel(
+      cep: map['cep'] ?? '',
+      logradouro: map['logradouro'] ?? '',
+      complemento: map['complemento'] ?? '',
+      bairro: map['bairro'] ?? '',
+      localidade: map['localidade'] ?? '',
+      uf: map['uf'] ?? '',
+      ibge: map['ibge'] ?? '',
+      gia: map['gia'] ?? '',
+      ddd: map['ddd'] ?? '',
+      siafi: map['siafi'] ?? '',
+    );
+  }
+
+  AddressEntity toEntity() {
+    return AddressEntity(
+      cep: cep ?? '',
+      logradouro: logradouro ?? '',
+      complemento: complemento ?? '',
+      bairro: bairro ?? '',
+      localidade: localidade ?? '',
+      uf: uf ?? '',
+      ibge: ibge ?? '',
+      gia: gia ?? '',
+      ddd: ddd ?? '',
+      siafi: siafi ?? '',
+    );
+  }
+}
